6cb7d827353b26fe5e17495b89469618ed6d4edb
DouglasSimoesM <douglas.simoesm@gmail.com>
2025-01-22 15:37:02 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/controller/GameController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -5,6 +5,7 @@ import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,5 @@ public class GameController {
         return result;
     }
 
-
+    
 }
